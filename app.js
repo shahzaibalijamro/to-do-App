96034215c7026d436c2e0b9c1c01069774914d50
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2024-03-22 20:26:40 +0500

    commands added

---
+++ app.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('.input');
+let div = document.querySelector('div');
+let todoList = [];
+function printTodo() {
+    div.innerHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+        div.innerHTML += '';
+    }
+}
